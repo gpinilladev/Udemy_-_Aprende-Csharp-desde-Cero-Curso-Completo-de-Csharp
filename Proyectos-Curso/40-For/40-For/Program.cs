5d708046fcbde7ccbbc946e2b4fadeeedb3c04ad
German Pinilla <gpinilladev@gmail.com>
2022-05-19 00:17:11 -0500

    Se agregan practicas de estructuras for, while, do while y try catch

--- Proyectos-Curso/40-For/40-For/Program.cs
+++ Proyectos-Curso/40-For/40-For/Program.cs
@@ -24,14 +24,18 @@ namespace _40_For
                 Console.WriteLine($"Value i => {i}");
             }
             // La sentencia imprimira los numero del 0 al 9
+
             Console.WriteLine("\n");
+
             // For loop reverse
             for (int i = 10; i > 0; i--)
             {
                 Console.WriteLine($"Value i => {i}");
             }
             // La sentencia imprimira los numero del 10 al 1
+
             Console.WriteLine("\n");
+
             // For loop break
             // Podemos romper la secuencia del for loop con la palabra break;
             // En el siguiente ejemplo se rompe la secuencia del for cuando i es igual a 4
@@ -41,6 +45,21 @@ namespace _40_For
                 if (i == 4)
                     break;
             }
+
+            Console.WriteLine("\n");
+
+            // For loop anidado
+            // Las buenas practicas dicen que en lo posible no debemos usar loops anidados
+            // Pero en algunas ocasiones necesitaremos de iterar una matriz y esta es la forma que expone el lenguaje
+            // Podemos hacer cuantos loops anidados querramos
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = i; j < 10; j++)
+                {
+                    Console.WriteLine($"i => {i}, j=> {j}");
+                }
+            }
+
 
         }
     }
